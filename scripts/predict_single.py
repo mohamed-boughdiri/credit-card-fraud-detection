b040236bddb1fdfbe52cc9ce10d0838aaa788a6a
Boughdiri Mohamed <mboughdiri5@gmail.com>
2026-08-07 22:23:35 +0100

    Delete scripts directory

--- scripts/predict_single.py
+++
@@ -1,4 +0,0 @@
-def predict_transaction(transaction_df, model, scaler, threshold=0.5):
-    scaled = scaler.transform(transaction_df)
-    prob = model.predict_proba(scaled)[:, 1]
-    return "Fraud" if prob >= threshold else "Not Fraud", prob
